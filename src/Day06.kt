e36c6e6a35c7d0035495bc7f1a13f46c67dd8dfb
Stanislav Dumchykov <mechanikea@gmail.com>
2023-12-06 09:16:07 +0200

    CHANGE Improved code of day 6 of 2023

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,34 +1,53 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val times = Regex("\\d+").findAll(input[0]).toList().map { it.value.toInt() }
-        val distances = Regex("\\d+").findAll(input[1]).toList().map { it.value.toInt() }
+    fun part1(input: List<String>): Long {
+        val parsed = input.map {
+            it.substringAfter(" ").trimStart().split(Regex("\\s+"))
+        }
+        val transformed = parsed[0].zip(parsed[1]) { a, b ->
+            Pair(a.toLong(), b.toLong())
+        }
 
-        var mulOfRaces = 1
-        times.forEachIndexed { index, i ->
-            var waysToWin = 0
-            for (c in 0..distances[index]) {
-                val distance = c * (i - c)
-                if (distance > distances[index]) waysToWin++
+        var mulOfRaces = 1L
+        transformed.forEach {
+            var waysToLose = 0
+
+            for (c in 0..it.first) {
+                val distance = c * (it.first - c)
+                if (distance <= it.second) waysToLose++
+                else break
             }
-            mulOfRaces *= waysToWin
+            for (c in (0..it.first).reversed()) {
+                val distance = c * (it.first - c)
+                if (distance <= it.second) waysToLose++
+                else break
+            }
+
+            mulOfRaces *= (it.first + 1) - waysToLose
         }
         return mulOfRaces
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val times = input[0].substringAfter(":").replace(" ", "").toLong()
         val distances = input[1].substringAfter(":").replace(" ", "").toLong()
 
-        var waysToWin = 0
+        var waysToLose = 0L
         for (c in 0..times) {
             val distance = c * (times - c)
-            if (distance > distances) waysToWin++
+            if (distance <= distances) waysToLose++
+            else break
+        }
+        for (c in (0..times).reversed()) {
+            val distance = c * (times - c)
+            if (distance <= distances) waysToLose++
+            else break
         }
 
-        return waysToWin
+        return (times + 1) - waysToLose
     }
 
     val input = readInput("Day06")
-    println(part1(input))
-    println(part2(input))
+
+    part1(input).println()
+    part2(input).println()
 }
