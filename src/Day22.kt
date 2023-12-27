6d2c4e7dab5de6d17f0d42a13a2445b6475d345b
Stanislav Dumchykov <mechanikea@gmail.com>
2023-12-27 13:43:35 +0200

    ADD Added solution for part 2 of day 22 of 2023

--- src/Day22.kt
+++ src/Day22.kt
@@ -31,14 +31,64 @@ fun main() {
         return count
     }
 
-
     fun part2(input: List<String>): Int {
-        return input.size
+        fun List<Set<Point3D>>.step(): Pair<List<Set<Point3D>>, Int> {
+            val new = mutableListOf<Set<Point3D>>()
+            val fallen = hashSetOf<Point3D>()
+            var a = 0
+
+            for (b in this.sortedBy { b -> b.minOf { it.z } }) {
+                var cb = b
+                while (true) {
+                    val down = cb.mapTo(hashSetOf()) { p -> p.copy(z = p.z - 1) }
+                    if (down.any { it in fallen || it.z <= 0 }) {
+                        new += cb
+                        fallen += cb
+                        if (cb != b) a++
+                        break
+                    }
+
+                    cb = down
+                }
+            }
+
+            return new to a
+        }
+
+        val bricks = input.map {
+            val coordinateValues = it.replace("~", ",").split(",").map { it.toInt() }
+            Brick(
+                coordinateValues[0]..coordinateValues[3],
+                coordinateValues[1]..coordinateValues[4],
+                coordinateValues[2]..coordinateValues[5]
+            )
+        }.sortedBy { it.z.first }.toMutableList()
+
+        val brickPoints = bricks.map { brick ->
+            val pointSet = hashSetOf<Point3D>()
+
+            for (x in brick.x.first..brick.x.last) {
+                for (y in brick.y.first..brick.y.last) {
+                    for (z in brick.z.first..brick.z.last) {
+                        pointSet += Point3D(x, y, z)
+                    }
+                }
+            }
+
+            pointSet
+        }
+
+        val (s) = brickPoints.step()
+        val t = s.map { b ->
+            val n = s.minusElement(b)
+            n.step().second
+        }
+        return t.sum()
     }
 
     val input = readInput("Day22")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
 private fun calculateSupportedBricks(
@@ -98,3 +148,5 @@ private data class Brick(var x: IntRange, var y: IntRange, var z: IntRange) {
         return false
     }
 }
+
+data class Point3D(val x: Int, val y: Int, val z: Int)
